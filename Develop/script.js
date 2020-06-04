44ec66a245f54322e9c27525d04a97bf83a2154e
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-06-04 15:34:09 -0500

    first commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,15 @@
 // Assignment code here
 
+//Create Function that prompts user for password criteria
+
+var generatePassword() 
+  //prompt how many characters the password needs to be, anywhere between 8 and 128
+
+  // prompt what character types need to be included - options should be lowercase, uppercase, numeric, and/or special characters, at least one needs to be selected
+
+  //password is generated and saved to passwordText.value 
+
+  
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
