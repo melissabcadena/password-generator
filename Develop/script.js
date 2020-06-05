5579956103820de26985746aeecf920aba10bc86
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-06-04 23:48:42 -0500

    add password requirements and validate response

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,27 @@
 var generatePassword = function () {
   //prompt how many characters the password needs to be, anywhere between 8 and 128
   promptLength();
-  
+
+  passwordRequirements();
+
+
+
+  //password is generated and saved to passwordText.value 
+}
+
+var promptLength = function () {
+  var characterLength = window.prompt ("How many characters would you like your password to be? Please choose a value between 8 and 128.")
+
+  if (characterLength < 8 || characterLength > 128) {
+    window.alert("Please choose a value between 8 and 128.");
+    promptLength();
+  }
+  else {
+    return characterLength;
+  }
+}
+
+var passwordRequirements = function () {
   // prompt do you want to include lower case characters
   var lowerCase = window.confirm("Do you want to include lower case characters in your password?");
 
@@ -20,18 +40,12 @@ var generatePassword = function () {
 
   // verify that at least one of those prompts was answered w/ a yes
 
-  //password is generated and saved to passwordText.value 
-}
-
-var promptLength = function () {
-  var characterLength = window.prompt ("How many characters would you like your password to be? Please choose a value between 8 and 128.")
-
-  if (characterLength < 8 || characterLength > 128) {
-    window.alert("Please choose a value between 8 and 128.");
-    promptLength();
+  if (lowerCase === false && upperCase === false && specialCharacters === false && numbers === false) {
+    window.alert("You need to choose at least one required character for your password. Try again!");
+    passwordRequirements();
   }
   else {
-    return characterLength;
+    return lowerCase, upperCase, specialCharacters, numbers;
   }
 }
   
@@ -52,5 +66,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-generatePassword();
