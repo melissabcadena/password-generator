596bf9e70b856a1a51c6bb9ef65c87bd0ad9f140
Melissa Cadena <melissabcadena@Melissas-MacBook-Pro.local>
2020-06-06 11:48:16 -0500

    added if statements to add at least one character per desired category

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,40 @@
 // Assignment code here
 
 //Create Function that prompts user for password criteria
-
+var password = "";
 var generatePassword = function () {
+  debugger;
   //prompt how many characters the password needs to be, anywhere between 8 and 128
   var characterLength = promptLength();
   
   console.log(characterLength);
   passwordRequirements();
-  console.log(desiredCharacters);
-  var password = "";
 
-  // for loop to create a string until it is the length of characterLength
-  for (var i=0; i < characterLength; i++) {
-  // if statement going through all 4 categories, if yes, add one character from the category to the password
-    if (lowerCase) {
-      var addCharacter = lower[randomNumber()];
-      password += addCharacter;
-      console.log(password);
-    }
+  if (lowerCase) {
+    var addCharacter = passwordArrays.lower[Math.floor(Math.random() * passwordArrays.lower.length)];
+    password += addCharacter;
   }
 
-    // nest if statements to add only characters they wanted inside their password
+  if (upperCase)  {
+    var addCharacter = passwordArrays.upper[Math.floor(Math.random() * passwordArrays.upper.length)];
+    password += addCharacter;
+  }
+
+  if (specialCharacters) {
+    var addCharacter = passwordArrays.special[Math.floor(Math.random() * passwordArrays.special.length)];
+    password += addCharacter;
+  }
+
+  if (numbers) {
+    var addCharacter = passwordArrays.number[Math.floor(Math.random() * passwordArrays.number.length)];
+    password += addCharacter;
+  }
+    // for loop to create a string until it is the length of characterLength
+  //for (var i=0; i < characterLength; i++) {
+  // if statement going through all 4 categories, if yes, add one character from the category to the password
+
+    //}
+  console.log(password);
 }
 
 var promptLength = function () {
@@ -46,16 +59,16 @@ var numbers;
 
 var passwordRequirements = function () {
   // prompt do you want to include lower case characters
-  var lowerCase = window.confirm("Do you want to include lower case characters in your password?");
+  lowerCase = window.confirm("Do you want to include lower case characters in your password?");
 
   // prompt do you want to inclue upper case characters
-  var upperCase = window.confirm("Do you want to include upper case characters?");
+  upperCase = window.confirm("Do you want to include upper case characters?");
 
   // prompt - special characters
-  var specialCharacters = window.confirm("Do you want to include special characters?");
+  specialCharacters = window.confirm("Do you want to include special characters?");
 
   // prompt - numbers
-  var numbers = window.confirm("Do you want to include numbers?");
+  numbers = window.confirm("Do you want to include numbers?");
 
   // verify that at least one of those prompts was answered w/ a yes
 
@@ -68,9 +81,7 @@ var passwordRequirements = function () {
   }
 }
 
-var randomNumber = function (arrayName) {
-  Math.floor(Math.random() * arrayName.length)
-};
+
   
 var passwordArrays = {
   lower: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
@@ -78,8 +89,6 @@ var passwordArrays = {
   special: ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', '|'],
   number: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 }
-
-  
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -94,5 +103,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-debugger;
 generateBtn.addEventListener("click", writePassword);
